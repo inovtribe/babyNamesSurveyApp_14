ed2a2f1725e26b00aa40da250817a8641fe8958c
karys4 <kary.github@gmail.com>
2018-11-04 13:04:58 -0600

    Baby names app commit

---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'HomeScreen.dart';
+
+void main() => runApp(new MaterialApp(
+      title: 'BabyNames Survey',
+      theme: new ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: new HomeScreen(),
+    ));
